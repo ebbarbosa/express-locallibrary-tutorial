639303899fad62e6598c10139109b9d5420893ac
Eduardo <ebbarbosa@users.noreply.github.com>
2018-01-04 19:30:40 -0200

    first commit
    coded up to this part https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/Displaying_data

---
+++ models/author.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema({
+    first_name: { type: String, required: true, max: 100 },
+    family_name: { type: String, required: true, max: 100 },
+    date_of_birth: { type: Date },
+    date_of_death: { type: Date },
+});
+
+AuthorSchema.virtual('name').get(function () {
+    return this.first_name + ' ' + this.family_name;
+});
+
+AuthorSchema.virtual('url').get(function () {
+    return '/catalog/author/' + this._id;
+});
+
+AuthorSchema.virtual('memento').get(function () {
+    var birth = this.date_of_birth ? moment(this.date_of_birth).format('MM-DD-YYYY') : '';
+    var death = this.date_of_death ? moment(this.date_of_death).format('MM-DD-YYYY') : '';
+    return birth + " - " + death;
+});
+
+
+module.exports = mongoose.model('Author', AuthorSchema);
